eeeef7d421bf9e2efe05c67aaea736d62d5b0d8a
Tolga <06thy40@gmail.com>
2022-07-10 14:56:05 +0300

    added newuser and adduser buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import mailSvg from "./assets/mail.svg";
-import manSvg from "./assets/man.svg";
+// import manSvg from "./assets/man.svg";
 import womanSvg from "./assets/woman.svg";
-import manAgeSvg from "./assets/growing-up-man.svg";
+// import manAgeSvg from "./assets/growing-up-man.svg";
 import womanAgeSvg from "./assets/growing-up-woman.svg";
 import mapSvg from "./assets/map.svg";
 import loadingGif from "./assets/loading.gif";
@@ -12,25 +12,27 @@ import cwSvg from "./assets/cw.svg";
 import Footer from "./components/footer/Footer";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import users from "./data";
 
 const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
-  const [isData, setIsData] = useState({});
+  const [isData, setIsData] = useState({
+    firstName: "",
+    email: "",
+    phone: "",
+    age: "",
+  });
   const [isShow, setIsShow] = useState(true);
-  const [show, setShow] = useState(false);
+  const [showName, setShowName] = useState(false);
+  const [showEmail, setShowEmail] = useState(false);
+  const [showAge, setShowAge] = useState(false);
+  const [showMap, setShowMap] = useState(false);
+  const [showPhone, setShowPhone] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  // const [info, setInfo] = useState({
-  //   title: "",
-  //   first: "",
-  //   last: "",
-  //   email: "",
-  //   age: "",
-  //   country: "",
-  //   cell: "",
-  //   gender: "",
-  // });
+  const [user, setUser] = useState(users);
 
   const getData = async () => {
     try {
@@ -57,15 +59,72 @@ function App() {
   const {
     name: { title, first, last },
     email,
-    registered: { age },
+    dob: { age },
     location: { country },
     cell,
-    gender,
-    picture: { thumbnail },
+    login: { password },
+    picture: { large },
   } = isData;
-  console.log(gender);
 
-  const handleName = () => {};
+  const handleName = () => {
+    setShowName(true);
+    setShowEmail(false);
+    setShowMap(false);
+    setShowAge(false);
+    setShowPhone(false);
+    setShowPassword(false);
+  };
+  const handleEmail = () => {
+    setShowName(false);
+    setShowEmail(true);
+    setShowMap(false);
+    setShowAge(false);
+    setShowPhone(false);
+    setShowPassword(false);
+  };
+  const handleAge = () => {
+    setShowName(false);
+    setShowEmail(false);
+    setShowMap(false);
+    setShowAge(true);
+    setShowPhone(false);
+    setShowPassword(false);
+  };
+  const handleMap = () => {
+    setShowName(false);
+    setShowEmail(false);
+    setShowMap(true);
+    setShowAge(false);
+    setShowPhone(false);
+    setShowPassword(false);
+  };
+  const handlePhone = () => {
+    setShowName(false);
+    setShowEmail(false);
+    setShowMap(false);
+    setShowAge(false);
+    setShowPhone(true);
+    setShowPassword(false);
+  };
+  const handlePassword = () => {
+    setShowName(false);
+    setShowEmail(false);
+    setShowMap(false);
+    setShowAge(false);
+    setShowPhone(false);
+    setShowPassword(true);
+  };
+  const handleAdd = () => {
+    setUser([
+      ...user,
+      {
+        firstName: isData.name.title + isData.name.first + isData.name.last,
+        email: isData.email,
+        age: isData.dob.age,
+        phone: isData.phone,
+      },
+    ]);
+  };
 
   return (
     <main>
@@ -75,37 +134,76 @@ function App() {
       <div className="block">
         <div className="container">
           <img
-            src={isShow ? defaultImage : thumbnail}
+            src={isShow ? defaultImage : large}
             alt="random user"
             className="user-img"
           />
-          <p className="user-title">My {title} is</p>
-          <p className="user-value"></p>
+          {showName && (
+            <div>
+              <p className="user-title"> My name is </p>
+              <p className="user-value">{title + first + last}</p>
+            </div>
+          )}
+          {showEmail && (
+            <div>
+              <p className="user-title">{showEmail && `My email is`}</p>
+              <p className="user-value">{showEmail && email}</p>
+            </div>
+          )}
+          {showAge && (
+            <div>
+              <p className="user-title"> My name is </p>
+              <p className="user-value">{age}</p>
+            </div>
+          )}
+          {showMap && (
+            <div>
+              <p className="user-title"> My name is </p>
+              <p className="user-value">{country}</p>
+            </div>
+          )}
+          {showPhone && (
+            <div>
+              <p className="user-title"> My name is </p>
+              <p className="user-value">{cell}</p>
+            </div>
+          )}
+          {showPassword && (
+            <div>
+              <p className="user-title"> My name is </p>
+              <p className="user-value">{password}</p>
+            </div>
+          )}
+
           <div className="values-list">
             <button className="icon" data-label="name" onClick={handleName}>
               <img src={womanSvg} alt="user" id="iconImg" />
             </button>
-            <button className="icon" data-label="email">
+            <button className="icon" data-label="email" onClick={handleEmail}>
               <img src={mailSvg} alt="mail" id="iconImg" />
             </button>
-            <button className="icon" data-label="age">
+            <button className="icon" data-label="age" onClick={handleAge}>
               <img src={womanAgeSvg} alt="age" id="iconImg" />
             </button>
-            <button className="icon" data-label="street">
+            <button className="icon" data-label="street" onClick={handleMap}>
               <img src={mapSvg} alt="map" id="iconImg" />
             </button>
-            <button className="icon" data-label="phone">
+            <button className="icon" data-label="phone" onClick={handlePhone}>
               <img src={phoneSvg} alt="phone" id="iconImg" />
             </button>
-            <button className="icon" data-label="password">
+            <button
+              className="icon"
+              data-label="password"
+              onClick={handlePassword}
+            >
               <img src={padlockSvg} alt="lock" id="iconImg" />
             </button>
           </div>
           <div className="btn-group">
-            <button className="btn" type="button">
+            <button className="btn" type="button" onClick={() => getData()}>
               new user
             </button>
-            <button className="btn" type="button">
+            <button className="btn" type="button" onClick={handleAdd}>
               add user
             </button>
           </div>
@@ -120,14 +218,17 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <tr className="body-tr">
-                <td>
-                  {title} {first} {last}
-                </td>
-                <td>{email}</td>
-                <td>{cell}</td>
-                <td>{age}</td>
-              </tr>
+              {user.map((person) => {
+                const { firstName, email, age, phone } = person;
+                return (
+                  <tr className="body-tr">
+                    <td>{firstName}</td>
+                    <td>{email}</td>
+                    <td>{phone}</td>
+                    <td>{age}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
